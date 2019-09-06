1bb094e646d6be54bc9af4b206341bf74923e870
Tatiana Acero Cuellar <taceroc@darkstar.org>
2019-09-06 12:29:33 -0500

    primos yo

---
+++ primos.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+  for(int j=2;j<=100;j++)
+    { 
+      int a=0;
+      for(int i=1;i<=100;i++)
+	{
+	  if(j%i==0)
+	    a++;
+	}
+      if(a==2)
+	{
+	  printf("%d\n",j);
+	}
+     }
+}
