07ce20fcbbb5fffa9f450cb82a6640b81f3b53cc
Tatiana Acero Cuellar <taceroc@darkstar.org>
2019-09-06 11:12:41 -0500

    ijhbjhjhjh jje

---
+++ 4-09/holamundo.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //imprimir pantalla
+//#using namespace std;: es mejor std:: dentro de std esta cout
+
+int main(void)
+
+{
+  double x = 1.0e-6;
+  std::cout<<"Hola Munduo\n";
+  // std :: cout<<"Hola Mundo"<<endl;
+  std::cout<<x<<"\n";
+  return 0;
+}
+
