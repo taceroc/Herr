2331ae343f448bc97ed4094e7c1a7ac69b958f4c
Tatiana Acero Cuellar <taceroc@darkstar.org>
2019-09-27 12:48:16 -0500

    Distribucion normal mas histograma usando funciones de c++

---
+++ 27-09/distribucionnormal.cpp
@@ -0,0 +1,30 @@
+#include <random>
+#include <iostream>
+#include <vector>
+ 
+int main()
+{
+  const int NSAMPLE = 10000;
+  int seed = 1;
+
+  const double xmin = 0.0;
+  const double xmax = 1.0;
+  const double dx = 0.01;
+  const int nbins = (xmax - xmin)/dx;
+  std::vector<double> histo(nbins);
+  
+  std::mt19937 gen(seed); 
+  std::normal_distribution<> d{0.5, 0.1}; //media, desviacion estandar
+  
+  for (int n = 0; n < NSAMPLE; ++n)
+    {
+      double r = d(gen);
+      int idx = int (r/dx);
+      histo[idx] = histo[idx] + 1;
+    }
+
+  for(int i = 0; i < nbins; i++)
+    {
+      std::cout << xmin + i * dx << "  " << histo[i]/NSAMPLE/dx<< "\n";
+    }
+}
